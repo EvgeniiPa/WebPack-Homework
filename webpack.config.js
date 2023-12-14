4987748ca9ea5c68d78c24f2fca36176224e8e40
Evgenii Pasechnikov <evgenii.pase4nickov@gmail.com>
2023-12-14 17:35:40 +0300

    Homework 14.12

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
